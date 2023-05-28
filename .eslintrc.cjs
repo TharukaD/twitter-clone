48a63e501c778b636f607bc4a3d66708702844c0
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-05-28 23:22:26 +0500

    Check123

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ const config = {
         "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/restrict-plus-operands": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
       },
       files: ["*.ts", "*.tsx"],
       parserOptions: {
