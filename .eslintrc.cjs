c2b396950ed39a43553449b547bc4ccb7e7508fb
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-05-28 23:20:38 +0500

    Changed ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,6 @@ const config = {
         "@typescript-eslint/no-unsafe-return": "off",
         "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/restrict-plus-operands": "off",
-        "@typescript-eslint/restrict-template-expressions": "error",
         "@typescript-eslint/no-non-null-assertion": "off",
       },
       files: ["*.ts", "*.tsx"],
